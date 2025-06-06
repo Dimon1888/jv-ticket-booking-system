76571a34b5dc2317b99d43b7dc2386b1026997b3
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-06-06 21:22:51 +0200

    Seat reservation task completed

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,23 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
 
+    private final Semaphore seats;
+
     public TicketBookingSystem(int totalSeats) {
+        this.seats = new Semaphore(totalSeats);
 
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        boolean gotSeats = seats.tryAcquire();
+        while (!gotSeats) {
+            return new BookingResult(user, false, "No seats available.");
+        }
+        int seatsLeft = seats.availablePermits();
+        String message = "Booking successful.";
+        return new BookingResult(user, true, message);
     }
 }
